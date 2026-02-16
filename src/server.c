2a2182c7c94a5ba39eb0af073423a45f2b12d31b
SlyCoder <phoebe.jing.ma@gmail.com>
2026-02-16 18:48:32 +0800

    first submit

---
+++ src/server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "unp.h"
+
+int main()
+{
+    printf("Hello, CMake with UCRT64!\n");
+    printf("哈哈哈哈!\n");
+    app_log();
+    return 0;
+}
---
+++ src/unp.c
@@ -0,0 +1,6 @@
+
+#include <stdio.h>
+void app_log()
+{
+    printf("zheshi ge ceshi");
+}
